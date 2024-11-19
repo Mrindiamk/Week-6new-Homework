f4f3e82f36dac83a4d62058f6960f3d1d6a40bc7
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-19 15:19:12 +0000

    Program2

---
+++ Week 6new Homework/src/Program2.java
@@ -0,0 +1,21 @@
+/* 2. Write a Java programme using the following steps.
+2.1 Declare two static variables.
+2.2 Declare one static method.
+2.3 Call both static variables into the static method inside the print statement.
+2.4 Declare the Main method.
+2.5 Call the static method into the Main method and run the programme.*/
+
+public class Program2 {
+
+    static String name = "Amin";
+    static int Age = 25;
+
+    public static void main(String[] args) {
+        sMethod();
+    }
+
+    public static void sMethod() {
+        System.out.println(name);
+        System.out.println(Age);
+    }
+}
