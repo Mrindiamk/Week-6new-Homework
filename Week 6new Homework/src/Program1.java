865754f6711d78a1d3eb70b7385e2b5607f84f29
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-19 14:43:40 +0000

    my first push

---
+++ Week 6new Homework/src/Program1.java
@@ -0,0 +1,28 @@
+public class Program1 {
+
+      String Name = "Amin"; //Instance Variable1
+      int Age = 25; //Instance Variable2
+
+    public static void main(String[] args) {
+
+     Program1 m= new Program1(); //Main Method
+        System.out.println(m.Name);
+        System.out.println(m.Age);
+
+
+
+    }
+
+
+    public void m1(){
+
+        System.out.println(Name); //Instance method
+        System.out.println(Age); //Instance Method
+
+
+
+
+    }
+
+
+}
