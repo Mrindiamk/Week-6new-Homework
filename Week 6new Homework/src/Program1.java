eb6fbd8f0a5299d88378795544b2dc420f969e71
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-19 15:01:12 +0000

    Program1

--- Week 6new Homework/src/Program1.java
+++ Week 6new Homework/src/Program1.java
@@ -3,26 +3,15 @@ public class Program1 {
       String Name = "Amin"; //Instance Variable1
       int Age = 25; //Instance Variable2
 
+    //Main Method
     public static void main(String[] args) {
-
-     Program1 m= new Program1(); //Main Method
-        System.out.println(m.Name);
-        System.out.println(m.Age);
-
-
-
+        Program1 m = new Program1(); //Constructor(Object)
+        m.m1();
     }
 
-
+    //Instance Method
     public void m1(){
-
-        System.out.println(Name); //Instance method
-        System.out.println(Age); //Instance Method
-
-
-
-
+        System.out.println(Name); // print statement
+        System.out.println(Age); // print statement
     }
-
-
 }
