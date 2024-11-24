32e31fb98f2925ab5387c0fb1f087a67c4341b32
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-24 14:42:57 +0000

    Program9

---
+++ Week 6new Homework/src/Program9.java
@@ -0,0 +1,8 @@
+/* 9. Write a program to convert the upper case to lower case.*/
+public class Program9 {
+    public static void main(String[] args) {
+        String name = "Hello Codebuster ";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
