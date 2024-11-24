f65b26887f2b71943661e369a2d4fc76e6a2ba56
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-24 16:11:20 +0000

    Program9

---
+++ Week 6new Homework/src/Program15.java
@@ -0,0 +1,10 @@
+/* 15. Write a Java program to convert a given string into lowercase.
+Sample Input: THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+Output: the quick brown fox jumps over the lazy dog.*/
+public class Program15 {
+    public static void main(String[] args) {
+        String name = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
