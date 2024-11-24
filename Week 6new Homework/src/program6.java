0e04a46824d3ae78af41fb0f97a34e837c59f3dd
Mrindiamk <k4kenishalearning@gmail.com>
2024-11-24 14:10:38 +0000

    Program4

---
+++ Week 6new Homework/src/program6.java
@@ -0,0 +1,13 @@
+/*6. Write a program to enter any radius value of the circle and find out the area. (Formula
+of Area A=PI*r*r). */
+public class program6 {
+    public static void main(String[] args) {
+        int radious;
+        double pi = 3.142;
+        radious = 5;
+
+        double Area = pi*radious*radious;
+        System.out.println("Area of circle is :"+ Area );
+    }
+
+}
